b4728788563a6c498fcdb2923255a4520b00276d
fsenore <fsenore@b0e3558a-1426-0410-83de-bf875247a1ac>
2011-12-18 18:28:15 +0000

    Updated version to 1.2.

--- trunk/src/utils.cpp
+++ trunk/src/utils.cpp
@@ -32,7 +32,7 @@
 #include "utils.h"
 
 wxString CUtils::applicationName = wxT("VVV");
-wxString CUtils::applicationVersion = wxT("1.11");
+wxString CUtils::applicationVersion = wxT("1.2");
 int CUtils::expectedDatabaseVersion = 17;
 int CUtils::firstUnicodeDatabaseVersion = 14;
 wxString CUtils::strucUpdateDbName = wxT("vvv-struct-update.fdb");
