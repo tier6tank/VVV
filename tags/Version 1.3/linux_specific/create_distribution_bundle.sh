266b3522c3a61a6803b114b497dd74692ec3472b
fsenore <fsenore@b0e3558a-1426-0410-83de-bf875247a1ac>
2016-09-14 09:42:39 +0000

    Version 1.3

---
+++ tags/Version 1.3/linux_specific/create_distribution_bundle.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# this script creates the distribution package for VVV
+
+PROGRAM_NAME="VVV"
+
+# change here the version number
+VERSION="1.3.0"
+
+# changest the file name based on the package being 32 or 64 bit
+NBITS=`getconf LONG_BIT`
+
+if [ $NBITS == 64 ]; then
+  mkdir ${PROGRAM_NAME}-${VERSION}-x86_64
+  rm ${PROGRAM_NAME}-${VERSION}-x86_64/*
+  cp -r ../installed_output/* ${PROGRAM_NAME}-${VERSION}-x86_64/
+else
+  mkdir ${PROGRAM_NAME}-${VERSION}-i386
+  rm ${PROGRAM_NAME}-${VERSION}-i386/*
+  cp -r ../installed_output/* ${PROGRAM_NAME}-${VERSION}-i386/
+fi
+
+if [ $NBITS == 64 ]; then
+  rm ${PROGRAM_NAME}-${VERSION}-x86_64.tar.gz
+  tar cvzf ${PROGRAM_NAME}-${VERSION}-x86_64.tar.gz ${PROGRAM_NAME}-${VERSION}-x86_64/
+  rm -r ${PROGRAM_NAME}-${VERSION}-x86_64
+else
+  rm ${PROGRAM_NAME}-${VERSION}-i386.tar.gz
+  tar cvzf ${PROGRAM_NAME}-${VERSION}-i386.tar.gz ${PROGRAM_NAME}-${VERSION}-i386/
+  rm -r ${PROGRAM_NAME}-${VERSION}-i386
+fi
+
+
